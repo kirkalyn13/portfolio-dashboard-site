71345acec49de8d37dbdde2772fb5673ead2ab69
Kirk Alyn Santos <kirkalyn13@gmail.com>
2021-06-18 23:23:47 +0800

    add chat to products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,22 @@
 import Talkies from './Talkies'
 
-const productsTitle = "Find Serving Sites Near You"
-const productsContent = "Using the Site GeoMap App, Identify your possible serving site within your range. Packed with Site Information, including the availability of 2G, 3G, and 4G technologies. It is a powerful tool for service assurance and testing functions to easily identify concerned sites with service feedback issues."
+const sitegeoTitle = "Find Serving Sites Near You"
+const sitegeoContent = "Using the Site GeoMap App, Identify your possible serving site within your range. Packed with Site Information, including the availability of 2G, 3G, and 4G technologies. It is a powerful tool for service assurance and testing functions to easily identify concerned sites with service feedback issues."
+const pingChatTitle = "Connect with your Network."
+const pingChatContent = 'With the Ping Chat App, connect and collaborate with your colleagues within your "Local Area Network". Share updates, snippets, insights, and troubleshooting recommendations with this local chat app suited for your business needs, to deliver fast and quality telecommunications service to your subscribers.'
+
 
 const Products = () => {
     return (
         <div>
-            <img src="/images/sitegeo_sample.png" className="img-products" width="90%" height="600" position="sticky" alt=""/> 
-            <Talkies title={productsTitle} content={productsContent} />
+            <div className="container-product">
+                <img src="/images/sitegeo_sample.png" className="img-products" width="70%" height="450" position="sticky" alt=""/> 
+                <Talkies title={sitegeoTitle} content={sitegeoContent} />
+            </div>
+            <div className="container-product">
+                <Talkies title={pingChatTitle} content={pingChatContent} />
+                <img src="/images/chat.png" className="img-products" width="70%" height="450" position="sticky" alt=""/> 
+            </div>
         </div>
     )
 }
